0669ace4574459c3f47bb0bafa0a2d7f5c4a1f50
Kami <51175640+hfurusho@users.noreply.github.com>
2020-01-30 13:19:06 -0800

    Set up express server.

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html-routes.js"));
+
+app.listen(PORT, () => {
+  if (!process.env.PORT) {
+    console.log(`App running on http://localhost:${PORT}`);
+  }
+});
